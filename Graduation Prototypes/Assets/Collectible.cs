173846e33396b14ad44083a58638fb7d6e78389c
Aaron Visschedijk <aaron.visschedijk@gmail.com>
2020-10-23 11:17:12 +0200

    Added minimal collectible prefab

---
+++ Graduation Prototypes/Assets/Collectible.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    public float speed;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            Object.Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 rotation = new Vector3(0f, speed, 0f);
+        transform.Rotate(rotation * Time.deltaTime);
+    }
+}
